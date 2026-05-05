fd02ed25427efb9e232a71e5cf486fc59696c29c
bthardman <bradley.hardman@hotmail.co.uk>
2026-05-05 16:41:17 +0100

    fix the bidding phase next player

--- server/server.ts
+++ server/server.ts
@@ -386,19 +386,20 @@ io.on('connection', (socket) => {
     let nextIdx = (playerIndex + 1) % room.players.length;
     let allBidded = true;
     for (let i = 0; i < room.players.length; i++) {
-      if (typeof room.players[i].bid !== 'number') {
-        allBidded = false;
-        nextIdx = i;
-        break;
-      }
-    }
+  const checkIdx = (playerIndex + 1 + i) % room.players.length;
+  if (typeof room.players[checkIdx].bid !== 'number') {
+    allBidded = false;
+    nextIdx = checkIdx;
+    break;
+  }
+}
 
-    if (allBidded) {
-      GameStateMachine.transition(room, 'tricks');
-      room.currentPlayer = room.firstPlayer;
-    } else {
-      room.currentPlayer = nextIdx;
-    }
+if (allBidded) {
+  GameStateMachine.transition(room, 'tricks');
+  room.currentPlayer = room.firstPlayer;
+} else {
+  room.currentPlayer = nextIdx;
+}
 
     io.to(roomId).emit('state_updated', room);
   });
